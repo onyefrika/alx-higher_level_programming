3d8e20247d744a24eb179cbfd596bab3293dc41b
onyefrika <jennyfaazubuko@gmail.com>
2022-07-06 21:41:11 +0100

    new

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
 """
-    Python script that adds all args to a Python List.
-    List is then saved to a file.
+This program take the file add_item.json, and add the
+parameters to the list inside this file.
+- If the file doesn't exist create it.
+- If no exist parameters do nothing or create the list if the file is empty.
 """
 
+from sys import argv
+from os.path import exists
 
-import sys
-import json
-import os.path
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-file = "add_item.json"
-json_list = []
+namefile = "add_item.json"
+argc = len(argv)
 
-if os.path.exists(file):
-    json_list = load_from_json_file(file)
+file_list = []
 
-for i in range(1, len(sys.argv)):
-    json_list.append(sys.argv[i])
+if exists(namefile):
+    file_list = load_from_json_file(namefile)
 
-save_to_json_file(json_list, file)
+if (argc == 1):
+    save_to_json_file(file_list, namefile)
+else:
+    for index in range(1, argc):
+        file_list.append(argv[index])
+    save_to_json_file(file_list, namefile)
